a91384c9ea181bcd7c300fd5655430e21c3d04de
B.Tajahamdi <btajahmadi@hotmail.com>
2018-08-25 10:38:04 +0430

    Tasks created

---
+++ Koy.Blog/gulpfile.js
@@ -0,0 +1,16 @@
+﻿{
+    var gulp = require('gulp');
+
+    gulp.task("copyBootstrap", function () {
+        gulp.src("node_modules/bootstrap/dist/**").
+            pipe(gulp.dest("wwwroot/lib/bootstrap"));
+    });
+    gulp.task("copyPopper.js", function () {
+        gulp.src("node_modules/popper.js/dist/**").
+            pipe(gulp.dest("wwwroot/lib/popper.js"));
+    });
+    gulp.task("copyJQuery", function () {
+        gulp.src("node_modules/jquery/dist/**").
+            pipe(gulp.dest("wwwroot/lib/jquery"));
+    });
+}
